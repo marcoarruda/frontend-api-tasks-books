43b5e0e7c439ac9d3ece72afd860ead773d4adb9
Marco Antonio Arruda <marco.nc.arruda@gmail.com>
2023-03-30 20:05:16 -0300

    test: add storybook test runner

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    // 'storybook-addon-module-mock',
+    'storybook-addon-module-mock',
     {
       name: '@storybook/addon-styling',
       options: {
