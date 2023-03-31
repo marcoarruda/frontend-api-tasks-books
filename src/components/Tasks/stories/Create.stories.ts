f55f9efa5c88482698f8116e53a5ea482de08340
Marco Antonio Arruda <marco.nc.arruda@gmail.com>
2023-03-30 21:22:14 -0300

    test: improve story assertions

--- src/components/Tasks/stories/Create.stories.ts
+++ src/components/Tasks/stories/Create.stories.ts
@@ -14,73 +14,82 @@ import { Task } from '../../../types/models';
 import { stubApiResponse, stubTask, stubTaskFormFailure } from '../../../api/__tests__/stubs';
 import { flushPromises } from '@vue/test-utils';
 
+const onCreateEmit = jest.fn()
 export default {
   title: 'CreateForm',
   component: CreateForm,
-  argTypes: {
-    onCreate: { action: true },
-  }
 } as Meta;
 
 const Template: Story = (args) => ({
   components: { CreateForm },
   setup() {
     return {
-      onCreate: action('onCreate',)
+      onCreate: (args: any) => {
+        onCreateEmit()
+        action('onCreate').call({}, args)
+      },
     }
   },
-  template: '<CreateForm @onCreate="onCreate" />'
+  template: '<CreateForm @onCreate="onCreate" />',
 })
 
 export const SubmitSuccess = Template.bind({})
-SubmitSuccess.play = async ({ args, canvasElement }) => {
+SubmitSuccess.play = async ({ args, canvasElement, }) => {
   const canvas = within(canvasElement)
 
   const stubSuccessResponse = stubApiResponse<Task, TaskCreateForm>(true, stubTask(), null)
-  const mock = createMock(apiTasks, 'createTask')
-  mock.mockResolvedValue(stubSuccessResponse)
+  const apiCreateTaskMock = createMock(apiTasks, 'createTask')
+  apiCreateTaskMock.mockResolvedValue(stubSuccessResponse)
 
   const taskTitulo = 'Wash the dishes'
 
   await userEvent.type(canvas.getByTestId('titulo'), taskTitulo, { delay: 50 });
 
   userEvent.click(canvas.getByRole('button'));
-  expect(mock).toHaveBeenCalledWith({ titulo: taskTitulo })
+  expect(apiCreateTaskMock).toHaveBeenCalledWith({ titulo: taskTitulo })
 
   await flushPromises()
+
+  expect(onCreateEmit).toHaveBeenCalled()
   await expect((canvas.getByTestId('titulo') as HTMLInputElement).value).toBe('')
 }
 
 export const SubmitFormError = Template.bind({})
 SubmitFormError.play = async ({ args, canvasElement }) => {
+  onCreateEmit.mockClear()
+
   const canvas = within(canvasElement)
 
   const stubFailureResponse = stubApiResponse<Task, TaskCreateForm>(false, stubTaskFormFailure(), null)
-  const mock = createMock(apiTasks, 'createTask')
-  mock.mockResolvedValue(stubFailureResponse)
+  const apiCreateTaskMock = createMock(apiTasks, 'createTask')
+  apiCreateTaskMock.mockResolvedValue(stubFailureResponse)
 
   const taskTitulo = 'Wat'
 
   await userEvent.type(canvas.getByTestId('titulo'), taskTitulo, { delay: 50 });
   await userEvent.click(canvas.getByRole('button'));
 
-  expect(mock).toHaveBeenCalledWith({ titulo: taskTitulo })
+  expect(apiCreateTaskMock).toHaveBeenCalledWith({ titulo: taskTitulo })
+  expect(onCreateEmit).not.toHaveBeenCalled()
   expect((canvas.getByTestId('titulo') as HTMLInputElement).value).toBe(taskTitulo)
 }
 
 export const SubmitApiError = Template.bind({})
 SubmitApiError.play = async ({ args, canvasElement }) => {
+  onCreateEmit.mockClear()
+
   const canvas = within(canvasElement)
 
   const stubFailureApi = stubApiResponse<Task, TaskCreateForm>(false, null, 'Something went wrong')
-  const mock = createMock(apiTasks, 'createTask')
-  mock.mockResolvedValue(stubFailureApi)
+  const apiCreateTaskMock = createMock(apiTasks, 'createTask')
+  apiCreateTaskMock.mockResolvedValue(stubFailureApi)
 
   const taskTitulo = 'Err'
 
   await userEvent.type(canvas.getByTestId('titulo'), taskTitulo, { delay: 50 });
   await userEvent.click(canvas.getByRole('button'));
 
-  expect(mock).toHaveBeenCalledWith({ titulo: taskTitulo })
+  expect(apiCreateTaskMock).toHaveBeenCalledWith({ titulo: taskTitulo })
+  expect(onCreateEmit).not.toHaveBeenCalled()
   expect((canvas.getByTestId('titulo') as HTMLInputElement).value).toBe(taskTitulo)
 }
